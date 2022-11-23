f76df2037af872684224467716aa4b65337ea88f
Shadowcrusher <58191113+Shadowcrusher@users.noreply.github.com>
2022-11-24 00:18:23 +0100

    Add files via upload

---
+++ oop/provjera2.cpp
@@ -0,0 +1,61 @@
+/* Napisati funkciju provjeri_zagrade() koja provjerava da li su zagrade
+ u nekom stringu korektno zatvorene, te shodno tome vraca tacno ili netacno. Primjer: 
+ { [ ( ) ] } -> True
+ { [ 3 * ( 2 + 3 ) + 2 ] } -> True
+ { [ ( ) } ] -> False */
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+bool provjeri_zagrade(string str) {
+
+  vector<char> otv;
+
+  for(int i = 0; i < str.length(); i++) {
+
+    if(str[i] == '(' || str[i] == '[' || str[i] == '{')
+      otv.push_back(str[i]);
+    else if(str[i] == ')')
+      if(otv.size())
+        if(otv.at(otv.size() - 1) == '(')
+          otv.pop_back();
+        else
+          return false;
+      else
+        return false;
+    else if(str[i] == ']')
+      if(otv.size())
+        if(otv.at(otv.size() - 1) == '[')
+          otv.pop_back();
+        else
+          return false;
+      else
+        return false;
+    else if(str[i] == '}')
+      if(otv.size())
+        if(otv.at(otv.size() - 1) == '{')
+          otv.pop_back();
+        else
+          return false;
+      else
+        return false;
+  }
+
+  return true;
+}
+
+int main() {
+  
+  string foo;
+
+  while(getline(cin, foo))
+    if(provjeri_zagrade(foo))
+      cout << "OK" << endl;
+    else
+      cout << "Error" << endl;
+
+  return 0;
+}
